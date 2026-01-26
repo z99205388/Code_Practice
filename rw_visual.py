e6e26b0359d6667e7e55ae0993db4688caa4178b
z99205388 <vampirezpzd@163.com>
2026-01-26 23:44:08 +0800

    隐藏坐标轴，matplotlib在3.4版本以后，plt.axes() 会返回一个新的坐标轴对象，会覆盖隐藏的坐标轴,所以需要将ax先创建对象,调用并隐藏

--- rw_visual.py
+++ rw_visual.py
@@ -9,8 +9,21 @@ while True:
     rw = Randomwalk()
     rw.fill_walk()
 
+    fig, ax = plt.subplots()
+
+    # 隐藏坐标轴
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,edgecolors='none', s=15)
+    
+    # 突出起点和终点
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+
+
     plt.show()
 
     keeprunning = input("Make another walk?(y/n)")
