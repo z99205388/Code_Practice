1c5868a3a448753c388e5b1acf892a48e4eb09ce
z99205388 <vampirezpzd@163.com>
2026-01-28 23:49:09 +0800

    修改为D6+D10的骰子进行投掷

--- dice_visual.py
+++ dice_visual.py
@@ -4,11 +4,11 @@ from die import Die
 
 # 创建一个D6
 die1 = Die()
-die2 = Die()
+die2 = Die(10)
 
 # 掷几次骰子，并将结果存储在一个列表中
 results = []
-for roll in range(1000):
+for roll in range(50000):
     result = die1.roll() + die2.roll()
     results.append(result)
 
@@ -23,10 +23,10 @@ for value in range(2, max_result+1):
 # 对结果进行可视化
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6  1000 times."
-hist.x_labels = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+hist.title = "Results of rolling a D6 and a D10 50000 times."
+hist.x_labels = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]
 hist.x_title = "Ressult"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6+D6', frequencies)
+hist.add('D6+D10', frequencies)
 hist.render_to_file('die_visual.svg')
