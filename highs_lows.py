c57a63962179fb2e522c9b92c7a4c187ae70746f
z99205388 <vampirezpzd@163.com>
2026-01-31 23:07:05 +0800

    增加死亡谷对应数据和缺失数据错误处理

--- highs_lows.py
+++ highs_lows.py
@@ -3,7 +3,7 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-filename = "sitka_weather_2014.csv"
+filename = "death_valley_2014.csv"
 
 #从文件中获取日期、最高气温和最低气温
 with open(filename) as f:
@@ -11,14 +11,17 @@ with open(filename) as f:
     header_row = next(reader)
     dates, highs,lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+                       
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -27,7 +30,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # 设置图形的格式
-plt.title("Daily high and low temperatures, 2014", fontsize=24)
+title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=24)
 plt.xlabel(' ', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
